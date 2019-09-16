c9fa03b8eee83e415b00404da3382b58eba8de48
julesodea <jules.odea@gmail.com>
2019-09-17 10:07:41 +1200

    Updates

---
+++ precache-manifest.5bee527b3a7d929f05db94ba5e5942df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "913127929772f6e7c58492557af500e8",
+    "url": "/blackestate-react/index.html"
+  },
+  {
+    "revision": "5c89145e3e7df636215b",
+    "url": "/blackestate-react/static/css/main.098ad037.chunk.css"
+  },
+  {
+    "revision": "d2f49701cdf4e16e2815",
+    "url": "/blackestate-react/static/js/2.c06ee85b.chunk.js"
+  },
+  {
+    "revision": "5c89145e3e7df636215b",
+    "url": "/blackestate-react/static/js/main.3d39a5ea.chunk.js"
+  },
+  {
+    "revision": "a9c54a2340649f2019d3",
+    "url": "/blackestate-react/static/js/runtime~main.69603425.js"
+  },
+  {
+    "revision": "3013229df69aa2c11c0310d55af95db6",
+    "url": "/blackestate-react/static/media/be.3013229d.png"
+  }
+]);
